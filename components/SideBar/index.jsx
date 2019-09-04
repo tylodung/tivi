983a502eecbb14f33c55cf047171d442df6878cc
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-09-04 16:53:42 +0700

    up

--- components/SideBar/index.jsx
+++ components/SideBar/index.jsx
@@ -12,18 +12,14 @@ class SideBar extends React.Component {
         return (
             <div className='sidebar'>
                 <Youtube {...this.props} />
-		<div className='sidebar__sponsor'>Sponsor <br />
+		<div className='sidebar__sponsor'> <br />
 		<script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' />
 		{ process.env.NODE_ENV === 'production' ? AdsenseSponsorAds : null }
 		{ process.env.NODE_ENV === 'production' ? AdsenseSponsorAdsEnds : null }
 		</div>
-                <div className='sidebar__title'>Important</div>
+                <div className='sidebar__title'></div>
 		<div className='sidebar__link'>
-		<a href='/about/'>Odayaka Web & Advertising</a>
-		<a href='https://odayaka.vn/nangluongmattroi'>Năng lượng mặt trời</a>
-		<a href='https://thuexehonda.com'>Thuê xe Honda Phú Quốc</a>
-		<a href='/payments/'>How to pay? Thanh toán</a>
-		<a href='/help'>Hướng dẫn sử dụng</a>
+		
 		</div>
             </div>
         );
